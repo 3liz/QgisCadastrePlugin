e07899843ad7dddc99ad5a1cae89729142123c27
Michael Douchin <mdouchin@3liz.com>
2013-11-05 18:17:19 +0100

    Export - amélioration des calculs des sommes

--- templates/proprietes_baties_sum.tpl.sql
+++ templates/proprietes_baties_sum.tpl.sql
@@ -6,10 +6,10 @@ CASE
   ELSE sum(pt.tse_bipevla)
 END AS revenucadastral,
 
-sum(pt.co_vlbaia) as co_vlbaia, sum(pt.co_bipevla) as co_bipevla,
-sum(pt.gp_vlbaia) as gp_vlbaia, sum(pt.gp_bipevla) as gp_bipevla,
-sum(pt.de_vlbaia) as de_vlbaia, sum(pt.de_bipevla) as de_bipevla,
-sum(pt.re_vlbaia) as re_vlbaia, sum(pt.re_bipevla) as re_bipevla
+Sum(Cast(pt.co_vlbaia * px.pexb / 100 AS numeric(10,2))) as co_vlbaia, sum(pt.co_bipevla) as co_bipevla,
+Sum(Cast(pt.gp_vlbaia * px.pexb / 100 AS numeric(10,2))) as gp_vlbaia, sum(pt.gp_bipevla) as gp_bipevla,
+Sum(Cast(pt.de_vlbaia * px.pexb / 100 AS numeric(10,2))) as de_vlbaia, sum(pt.de_bipevla) as de_bipevla,
+Sum(Cast(pt.re_vlbaia * px.pexb / 100 AS numeric(10,2))) as re_vlbaia, sum(pt.re_bipevla) as re_bipevla
 FROM
 parcelle p
 INNER JOIN local00 l ON l.parcelle = p.parcelle
--- templates/proprietes_non_baties_sum.tpl.sql
+++ templates/proprietes_non_baties_sum.tpl.sql
@@ -4,7 +4,7 @@ sum(s.drcsuba) AS drcsuba
 
 FROM parcelle p
 INNER JOIN suf s ON p.parcelle = s.parcelle
-LEFT JOIN sufexoneration se ON s.suf = se.suf
+--LEFT JOIN sufexoneration se ON s.suf = se.suf
 WHERE 2>1
 $and
 
