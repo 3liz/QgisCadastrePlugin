709145eb62463498b86fc53ba03cd2dfdab55bb9
Michael Douchin <mdouchin@3liz.com>
2016-01-30 10:12:43 +0100

    [bugfix] Import - parcelles dupliquées si plusieurs sous-sections

--- scripts/opencadastre/trunk/data/pgsql/2015/edigeo_formatage_donnees.sql
+++ scripts/opencadastre/trunk/data/pgsql/2015/edigeo_formatage_donnees.sql
@@ -66,9 +66,10 @@ INSERT INTO [PREFIXE]geo_parcelle
 (geo_parcelle, annee, object_rid, idu, geo_section, geo_subdsect, supf, geo_indp, coar, tex, tex2, codm, creat_date, update_dat, geom, lot)
 SELECT '[ANNEE]'||'[DEPDIR]'||p.idu, '[ANNEE]', p.object_rid, p.idu, '[ANNEE]'||SUBSTRING(p.idu,1,8), s.geo_subdsect, p.supf, p.indp, p.coar, p.tex, p.tex2, p.codm, to_date(to_char(p.creat_date,'00000000'), 'YYYYMMDD'), to_date(to_char(p.update_date,'00000000'), 'YYYYMMDD'), ST_Multi(ST_CollectionExtract(ST_MakeValid(p.geom),3)), '[LOT]'
 FROM [PREFIXE]parcelle_id AS p
-LEFT JOIN edigeo_rel AS r ON r.nom='Rel_PARCELLE_SUBDSECT' AND r.de = p.object_rid
-LEFT JOIN geo_subdsect AS s ON s.annee = '[ANNEE]' AND r.vers = s.object_rid
+LEFT JOIN [PREFIXE]edigeo_rel AS r ON r.nom='Rel_PARCELLE_SUBDSECT' AND r.de = p.object_rid
+LEFT JOIN [PREFIXE]geo_subdsect AS s ON s.annee = '[ANNEE]' AND r.vers = s.object_rid
 WHERE p.idu IS NOT NULL
+AND '[ANNEE]'||SUBSTRING(p.idu,1,8) =  s.geo_section
 ;
 DROP INDEX IF EXISTS [PREFIXE]geo_subdsect_annee_idx;
 DROP INDEX IF EXISTS [PREFIXE]geo_subdsect_object_rid_idx;
