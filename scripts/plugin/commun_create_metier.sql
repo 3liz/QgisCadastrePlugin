37851b38d70a37c737600f71930254bff2cfb70c
Michael Douchin <mdouchin@3liz.com>
2017-11-28 17:06:47 +0100

    Import - Ajout de la nomenclature pour ccocac

--- scripts/plugin/commun_create_metier.sql
+++ scripts/plugin/commun_create_metier.sql
@@ -731,6 +731,7 @@ CREATE TABLE annul (annul text primary key, annul_lib text);
 CREATE TABLE typvoi (typvoi text primary key, typvoi_lib text);
 CREATE TABLE indldnbat (indldnbat text primary key, indldnbat_lib text);
 CREATE TABLE dformjur (dformjur text primary key, formjur text, libformjur text);
+CREATE TABLE ccocac (ccocac text primary key, ccocac_lib text);
 
 CREATE TABLE geo_commune
 (
