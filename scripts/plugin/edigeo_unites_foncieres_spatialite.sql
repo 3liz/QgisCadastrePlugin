c4ad251228939dc127d500f973ed2db76305cf67
MaelREBOUX <m.reboux@rennesmetropole.fr>
2020-08-18 17:27:46 +0200

    maj création des unités foncières
    # fix 221
    convergence des 2 requêtes pour gérer les parcelles du plan sans correspondance dans la matrice foncière
    
    testé

--- scripts/plugin/edigeo_unites_foncieres_spatialite.sql
+++ scripts/plugin/edigeo_unites_foncieres_spatialite.sql
@@ -1,8 +1,13 @@
 -- Création des unités foncières pour Spatialite
 INSERT INTO geo_unite_fonciere (comptecommunal, annee, lot, geom)
-SELECT comptecommunal, '[ANNEE]' AS annee, '[LOT]' AS lot, CastToMultiPolygon(st_unaryunion(st_collect(a.geom))) as geom
-FROM v_geo_parcelle a
-WHERE 2>1
-AND comptecommunal IS NOT NULL
-AND ST_IsValid(a.geom)
-GROUP BY comptecommunal
+  SELECT 
+    comptecommunal,
+    '[ANNEE]', '[LOT]', 
+    CastToMultiPolygon(st_unaryunion(st_collect(a.geom))) as geom
+  FROM v_geo_parcelle a
+  WHERE
+    a.annee = '[ANNEE]'
+    AND a.lot = '[LOT]'
+    AND ST_IsValid(a.geom)
+    AND comptecommunal IS NOT NULL
+    GROUP BY comptecommunal;
