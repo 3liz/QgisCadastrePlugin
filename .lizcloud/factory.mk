4f9694c72739201e0b2644e2995f71f4774efb93
David Marteau <dmarteau@3liz.com>
2020-11-04 14:18:10 +0100

    Fix infrav3 server packaging

--- .lizcloud/factory.mk
+++ .lizcloud/factory.mk
@@ -1,25 +1,29 @@
 #
 # Makefile for building/packaging qgis for lizmap hosting
 #
+.PHONY: files
 
-VERSION=$(shell ./metadata_key ../metadata.txt version)
+VERSION=$(shell ./metadata_key ../cadastre/metadata.txt version)
 
 main:
 	echo "Makefile for packaging infra components: select a task"
 
 PACKAGE=qgis_cadastre
-FILES = composers server forms icons interface scripts styles templates *.py *.qrc icon.png metadata.txt README.md
+FILES = cadastre/* README.md
 PACKAGEDIR=cadastre
+
+clean:
+	@rm -rf build/$(PACKAGEDIR)
 
 build/$(PACKAGEDIR):
 	@rm -rf build/$(PACKAGEDIR)
 	@mkdir -p build/$(PACKAGEDIR)
-
-
-.PHONY: package
-package: build/$(PACKAGEDIR)
 	@echo "Building package $(PACKAGE)"
 	@cd .. &&  cp -rLp $(FILES) .lizcloud/build/$(PACKAGEDIR)/
+
+files: clean build/$(PACKAGEDIR)
+
+package: files
 	$(FACTORY_SCRIPTS)/make-package $(PACKAGE) $(VERSION) $(PACKAGEDIR) ./build
 
 version:
