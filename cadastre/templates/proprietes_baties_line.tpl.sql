c4a43807b701ffa86616e1d50310e10700269284
MaelREBOUX <m.reboux@rennesmetropole.fr>
2021-03-11 12:42:38 +0100

    fix #272
    pb création chaîne adresse

--- cadastre/templates/proprietes_baties_line.tpl.sql
+++ cadastre/templates/proprietes_baties_line.tpl.sql
@@ -1,17 +1,14 @@
-
 SELECT
 l.ccosec AS section, ltrim(l.dnupla, '0') AS ndeplan,
 ltrim(l.dnvoiri, '0') || l.dindic AS ndevoirie,
-CASE WHEN v.libvoi IS NOT NULL THEN v.natvoi || v.libvoi ELSE p.cconvo || p.dvoilib END AS adresse,
+CASE WHEN v.libvoi IS NOT NULL THEN v.natvoi || ' ' || v.libvoi ELSE p.cconvo || p.dvoilib END AS adresse,
 l.ccoriv AS coderivoli,
 l.dnubat AS bat, l.descr AS ent, l.dniv AS niv, l.dpor AS ndeporte, substring(l.invar, 4, length(l.invar)) || ' ' || l.cleinvar AS numeroinvar,
 pev.ccostb AS star, l10.ccoeva AS meval, pev.ccoaff AS af, l10.cconlc AS natloc, pev.dcapec AS cat,
-
 CASE
   WHEN px.rcexba2 > 0 THEN px.rcexba2
   ELSE pt.tse_bipevla
 END AS revenucadastral,
-
 px.ccolloc AS coll, px.gnextl AS natexo, px.janimp AS anret, px.jandeb AS andeb, Cast(px.rcexba2 * px.pexb / 100 AS numeric(10,2)) AS fractionrcexo,
 px.pexb AS pourcentageexo, l10.gtauom AS txom, '' AS coefreduc
 FROM
--- cadastre/templates/proprietes_non_baties_line.tpl.sql
+++ cadastre/templates/proprietes_non_baties_line.tpl.sql
@@ -1,8 +1,7 @@
-SELECT --p.parcelle,
+SELECT
 p.ccosec AS section, ltrim(p.dnupla, '0') AS ndeplan,
 ltrim(p.dnvoiri, '0') || p.dindic AS ndevoirie,
-
-CASE WHEN v.libvoi IS NOT NULL THEN v.natvoi || v.libvoi ELSE p.cconvo || p.dvoilib END AS adresse,
+CASE WHEN v.libvoi IS NOT NULL THEN v.natvoi || ' ' || v.libvoi ELSE p.cconvo || p.dvoilib END AS adresse,
 p.ccoriv AS coderivoli,
 p.dparpi AS nparcprim, p.gparnf AS fpdp, s.ccostn AS star, s.ccosub AS suf, s.cgrnum || '/' || s.dsgrpf AS grssgr, s.dclssf AS cl, s.cnatsp AS natcult,
 CASE WHEN length(Cast(s.dcntsf AS text)) > 4 THEN substring(Cast(s.dcntsf AS text), 0, length(Cast(s.dcntsf AS text))-3) ELSE '0' END AS ha_contenance,
@@ -13,7 +12,6 @@ s.drcsuba AS revenucadastral, se.ccolloc AS coll, se.gnexts AS natexo, se.jfinex
 round(Cast(s.drcsuba * Cast(se.pexn / 100 AS numeric(10,2)) / 100 AS numeric) , 2) as fractionrcexo,
 Cast(se.pexn / 100 AS numeric(10,2)) AS pourcentageexo, '' AS tc,
 p.dreflf AS lff
-
 FROM $schema"parcelle" p
 INNER JOIN $schema"suf" s ON p.parcelle = s.parcelle
 LEFT OUTER JOIN $schema"voie" v ON v.voie = p.voie
