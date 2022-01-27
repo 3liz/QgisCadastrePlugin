cd5ea87d8916b36af76814976c108385cc235aeb
Etienne Trimaille <gustrimaille@yahoo.fr>
2022-01-27 09:22:09 +0100

    Server - refactor some code

---
+++ cadastre/server/__init__.py
@@ -0,0 +1,3 @@
+__copyright__ = 'Copyright 2022, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
--- cadastre/server/cadastre_server.py
+++ cadastre/server/cadastre_server.py
@@ -1,29 +1,15 @@
-"""
-Cadastre - QGIS plugin menu class
-
-This plugins helps users to import the french land registry ('cadastre')
-into a database. It is meant to ease the use of the data in QGIs
-by providing search tools and appropriate layer symbology.
-
-begin    : 2013-06-11
-copyright: (C) 2013 by 3liz
-email    : info@3liz.com
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-"""
+__copyright__ = 'Copyright 2022, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
 
 import os
 import tempfile
 
 from pathlib import Path
 
-from qgis.core import Qgis, QgsMessageLog
 from qgis.server import QgsServerInterface
 
+from cadastre.logger import Logger
 from cadastre.server.cadastre_service import CadastreService
 from cadastre.server.tools import version
 
@@ -36,26 +22,25 @@ class CadastreServer:
 
     def __init__(self, server_iface: QgsServerInterface) -> None:
 
-        QgsMessageLog.logMessage('Init server version "{}"'.format(version()), 'cadastre', Qgis.Info)
+        Logger.info('Init server version "{}"'.format(version()))
 
         cache_dir_str = os.getenv('QGIS_CADASTRE_CACHE_DIR')
         if not cache_dir_str:
             # Create cache in /tmp/org.qgis.cadastre
             cache_dir_str = os.path.join(tempfile.gettempdir(), 'org.qgis.cadastre')
 
-        self.cachedir = Path(cache_dir_str)
-        self.cachedir.mkdir(mode=0o750, parents=True, exist_ok=True)
+        self.cache_dir = Path(cache_dir_str)
+        self.cache_dir.mkdir(mode=0o750, parents=True, exist_ok=True)
 
-        QgsMessageLog.logMessage('Cache directory set to {}'.format(cache_dir_str), 'cadastre', Qgis.Info)
-
-        debug = os.getenv('QGIS_CADASTRE_DEBUG', '').lower() in ('1', 'yes', 'y', 'true')
+        Logger.info('Cache directory set to {}'.format(cache_dir_str))
 
         reg = server_iface.serviceRegistry()
-        reg.registerService(CadastreService(cachedir=self.cachedir, debug=debug))
+        reg.registerService(CadastreService(cache_dir=self.cache_dir))
 
     def createService(self, debug: bool = False) -> CadastreService:
         """ Create  a new service instance
 
             Used for testing
         """
-        return CadastreService(self.cachedir, debug=debug)
+        _ = debug
+        return CadastreService(self.cache_dir)
